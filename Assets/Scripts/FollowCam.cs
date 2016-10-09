7c05abce6253e0798ecf2703b778709e8c405565
dc820 <dc820@scarletmail.rutgers.edu>
2016-10-09 17:11:04 -0400

    Created FollowCam.cs and added Physics Mat for Projectile

---
+++ Assets/Scripts/FollowCam.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowCam : MonoBehaviour
+{
+    static public FollowCam S; // a follow cam singleton
+    //fields set in Unity inspector pane
+    public float easing = 0.05f;
+    public Vector2 minXY;
+    public bool ______________________;
+
+    //fields set dynamically
+    public GameObject poi; //The point of interest
+    public float camZ; //The desired Z pos of the camera
+
+
+    void Awake()
+    {
+        S = this;
+        camZ = this.transform.position.z;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate ()
+    {
+        // if there's only one line following an if, it doesn't need braces
+        if (poi == null) return; //return if there is no point
+
+        //Get the position of the poi
+        Vector3 destination = poi.transform.position;
+        //Limit the X & Y to minimum values
+        destination.x = Mathf.Max(minXY.x, destination.x);
+        destination.y = Mathf.Max(minXY.y, destination.y);
+        //Interpolate from the current Camera position toward destination
+        destination = Vector3.Lerp(transform.position, destination, easing);
+        //Retain a destination.z of camZ
+        destination.z = camZ;
+        //Set the camera to the destination
+        transform.position = destination;
+        //Set the orthographicSIze of the Camera to keep Ground in view
+        this.GetComponent<Camera>().orthographicSize = destination.y + 10;
+	}
+}
